9ff278d38c9736c815feac7dcda8eae78b8d11fa
Zhibul-Alexander <zhibul.alexander@gmail.com>
2026-01-05 00:17:49 +0400

    add snow animation

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/site/header";
 import { Footer } from "@/components/site/footer";
 import { ScrollToTop } from "@/components/site/scroll-to-top";
 import { ScrollToTopOnNavigate } from "@/components/site/scroll-to-top-on-navigate";
+import { Snowfall } from "@/components/site/snowfall";
 import { getSite } from "@/lib/content";
 import { LANGS, type Lang, langToHtmlLang } from "@/lib/i18n";
 
@@ -75,6 +76,7 @@ export default async function LangLayout({
       />
       <ScrollToTop />
       <ScrollToTopOnNavigate />
+      <Snowfall count={90} />
     </>
   );
 }
