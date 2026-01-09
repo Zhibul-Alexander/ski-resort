681bcb13adb656ecf8c9ed1b1f01c70dfc6c52ed
Zhibul-Alexander <zhibul.alexander@gmail.com>
2026-01-09 19:49:19 +0400

    add new animation

--- components/site/sunny-ski-backdrop.tsx
+++ components/site/sunny-ski-backdrop.tsx
@@ -7,6 +7,30 @@ export function SunnySkiBackdrop({ className }: { className?: string }) {
   return (
     <div className={cn("sunny-ski-backdrop", className)} aria-hidden="true">
       <div className="sunny-ski-bg" />
+      <div className="sunny-ski-clouds">
+        <div className="sunny-ski-cloud sunny-ski-cloud-1" />
+        <div className="sunny-ski-cloud sunny-ski-cloud-2" />
+        <div className="sunny-ski-cloud sunny-ski-cloud-3" />
+        <div className="sunny-ski-cloud sunny-ski-cloud-4" />
+        <div className="sunny-ski-cloud sunny-ski-cloud-5" />
+        <div className="sunny-ski-cloud sunny-ski-cloud-6" />
+        <div className="sunny-ski-cloud sunny-ski-cloud-7" />
+      </div>
+      <div className="sunny-ski-snow">
+        {Array.from({ length: 180 }).map((_, i) => (
+          <div
+            key={i}
+            className="sunny-ski-snowflake"
+            style={{
+              left: `${(i * 3.7) % 100}%`,
+              animationDelay: `${(i * 0.05) % 8}s`,
+              animationDuration: `${8 + (i % 12)}s`,
+              width: `${3 + (i % 3)}px`,
+              height: `${3 + (i % 3)}px`,
+            }}
+          />
+        ))}
+      </div>
       <div className="sunny-ski-sun-glare" />
       <div className="sunny-ski-haze" />
     </div>
